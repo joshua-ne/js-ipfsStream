b327cd27c5d6c4c2afb4ec6f6ef3012d07f45dbd
junyaoren <renjunyao@gmail.com>
2018-10-14 00:08:48 -0700

    use local ipfs gateway instead of cloudflare

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const IPFSFactory = require('ipfsd-ctl');
+
+console.log('123');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const f = IPFSFactory.create();
+
+f.spawn(function (err, ipfsd) {
+    if (err) { throw err }
+
+
+    let gateway = [];
+    ipfsd.api.id(function (err, id) {
+        if (err) { throw err }
+        gateway[0] = ipfsd._gatewayAddr.toString().split('/')[2];
+        gateway[1] = ipfsd._gatewayAddr.toString().split('/')[4];
+        //ipfsd.stop()
+        let ipfsGateway = gateway[0] + ':' + gateway[1];
+
+        router.get('/', function(req, res, next) {
+            res.render('index', { gateway: ipfsGateway});
+        });
+
+
+
+
+    })
 });
 
+
+
 module.exports = router;
+
+
